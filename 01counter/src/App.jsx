e0e9031a51b656daeb464875356c4ddd4e21fdd2
Arif Ansari <arifansari90467@gmail.com>
2025-04-19 17:13:22 +0530

    add reset button

--- 01counter/src/App.jsx
+++ 01counter/src/App.jsx
@@ -13,6 +13,10 @@ function App() {
     setCount(count-1);
   }
 
+  const resetValue = () => {
+    setCount(0);
+  }
+
   return (
     <div className='border bg-amber-100 p-4 justify-between items-center'>
       <h1 className='font-bold text-2xl'>Counter</h1>
@@ -20,9 +24,11 @@ function App() {
       <br />
 
       <div className="flex gap-4 justify-center">
-    <Button onClick={addValue} className="bg-blue-500 text-white px-4 py-2">Increment</Button>
-    <Button onClick={removeValue} className="bg-blue-500 text-white px-4 py-2">Decrement</Button>
+    <Button onClick={addValue} className="bg-blue-500 text-white px-4 py-2 cursor-pointer">Increment</Button>
+    <Button onClick={removeValue} className="bg-blue-500 text-white px-4 py-2 cursor-pointer">Decrement</Button>
     </div>
+    <br />
+    <Button onClick={resetValue} className="cursor-pointer">Reset</Button>
   </div>
   )
 }
